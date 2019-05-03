89d16498846d2c87c00fc4e1fb37b41048590fed
Luis Díaz Venero <luis@MacBook-Pro-de-Luis.local>
2019-05-02 20:55:15 -0500

    seteando graphiql

--- schema.js
+++ schema.js
@@ -1,14 +1,15 @@
-const { makeSchemaExecutable } = require('graphql-tools')
+const { makeExecutableSchema } = require('graphql-tools')
 
-const schema = `
+const typeDefs = `
   type Curso {
     id: ID!
     titulo: String!
   }
+  type Query {
+    cursos: [Curso]
+  }
 `
 
-const schema = makeSchemaExecutable({
-  typeDefs: schema
-})
+const schema = makeExecutableSchema({ typeDefs })
 
 module.exports = schema
